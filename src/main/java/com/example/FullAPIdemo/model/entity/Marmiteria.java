de2d38f269b5a0b08b3b9aeb4b26c1530794ef60
mariacfelix <mariacfelix07@gmail.com>
2026-09-02 00:52:14 -0300

    adicionado informações de endereço da marmiteria

--- src/main/java/com/example/FullAPIdemo/model/entity/Marmiteria.java
+++ src/main/java/com/example/FullAPIdemo/model/entity/Marmiteria.java
@@ -39,6 +39,50 @@ public class Marmiteria {
     @Column(nullable = true)
     private LocalDateTime createdAt;
 
+    @Column
+    private String rua;
+
+    @Column
+    private String bairro;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Column
+    private String cidade;
+
+    @Column
+    private String uf;
+
     @OneToMany(mappedBy = "marmiteria", cascade = CascadeType.ALL)
     @JsonIgnore
     private List<Pedido> pedidos;
